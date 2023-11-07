4180e2f4122dac2777ebf74c6a3827aaff03c59e
Teche <techellipticaeducation@gmail.com>
2023-11-07 08:56:15 +0530

    updates

--- src/testometer_nov_23_java_wd/Test2.java
+++ src/testometer_nov_23_java_wd/Test2.java
@@ -1,5 +1,33 @@
 package testometer_nov_23_java_wd;
 
 public class Test2 {
-
+public static void main(String[] args) {
+	// keyword - 
+	// keyword - which is having some predefined meaning (logic)
+	// eclipse - red and bullet ()
+	// package, public, class, static, void
+	// small letters
+	// any variable or any class, any method we cannot use in java
+	
+	// variable = vary + able = 
+	// constant value
+	final int age = 10;
+//	age = 20;
+//	age = 30;
+//	age = 40;
+	System.out.println(age);
+	// Data Type ? = data type
+	//  what is your age ?  =  27 (numerical) + - * / %
+	// what did you eat last night = rice (string)
+	// are you hungry ? yes/no (boolean)
+	// what is the interest rate for bank loan = 6.5
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
