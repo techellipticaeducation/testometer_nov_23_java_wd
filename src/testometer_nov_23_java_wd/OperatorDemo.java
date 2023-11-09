12de69ec322cd990be4d63c4d7c5bcce4ffeb0b4
Teche <techellipticaeducation@gmail.com>
2023-11-09 08:45:25 +0530

    updates

---
+++ src/testometer_nov_23_java_wd/OperatorDemo.java
@@ -0,0 +1,27 @@
+package testometer_nov_23_java_wd;
+
+public class OperatorDemo {
+	public static void main(String[] args) {
+		// Arithmatic Operators
+		// + 
+		// -
+		// *
+		// /
+		// %
+
+		int i = 10;
+		int j = 7;
+
+		System.out.println(i+j); // 17
+		System.out.println(i-j); // 3
+		System.out.println(i*j); // 70
+		System.out.println((float)i/(float)j); // 1.433423
+		System.out.println(i%j); // 3
+
+		
+		// odd and even 
+		// 12%2 = 0 (even)
+		// 11%2 = 1 (odd)
+
+	}
+}
---
+++ src/testometer_nov_23_java_wd/UnaryOperatorDemo.java
@@ -0,0 +1,56 @@
+package testometer_nov_23_java_wd;
+
+public class UnaryOperatorDemo {
+	public static void main(String[] args) {
+		// unary operator carry one oparend
+		// + , - , ++ , -- , !
+		
+//		boolean b1  = false;
+//		System.out.println(!b1);
+		
+		//System.out.println(+10);
+		
+		// increment operator
+		
+		
+//		++i;
+//		System.out.println(i); // 11
+		
+		// 2 operator , increment , assignment
+		// increment ++ ,  
+		// pre increment  ++i, first increase then assignment
+		// post increment i++, first assignment then increase
+		
+		int i = 10;
+//		int j =    ++i    +    ++i;
+	// i value//   11           12
+	//assign  //   11     +     12
+	   
+		
+		i+=2;
+		i-=2;
+		i*=2; // i = i * 2;
+		i/=2; // i = i / 2;
+		i%=2; // i = i % 2;
+		
+//		System.out.println("j -> " + j);
+		System.out.println("i -> " + i);
+		
+		
+		// increase value by 2
+		
+//		i = i +2;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
